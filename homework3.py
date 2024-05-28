f5e73c1fa4cf7fd94c4699c3f5fb358577b3c2c1
Kong0st <kongost@yandex.ru>
2024-05-28 12:45:59 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,17 @@
+name = 'Сергей'
+print (name, type(name))
+age = 29
+print(age, type (age))
+new_age = 29.5
+print(new_age, type (new_age))
+is_student = True
+print(is_student)
+print(" ")
+name = 'Сергей'
+print ("Имя студента: ", name)
+age = 29
+print("Количество лет: ", age, " лет (года)")
+new_age = 29.5
+print("Точный возраст: ", new_age, " лет (года)")
+is_student = True
+print("Студент: Верно", "(",is_student,")")
